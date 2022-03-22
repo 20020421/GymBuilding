6443fb6b32f2076fdc56eb60d5a11087fe8964a0
20020421 <hungkaiken2@gmail.com>
2022-03-22 19:06:07 +0700

    hungdz

--- js/main.js
+++ js/main.js
@@ -4,16 +4,8 @@ function addClassActive(element) {
 }
 const tabContents = document.getElementsByClassName("nav-link");
 const tables = document.getElementsByClassName("tab-hide");
-console.log(tables)
-// function toggleTable() {
-    
-//     for (const table of tabContents) {
-//         table.addEventListener('click', addClassActive(table)); 
-//     }
-// }
-
-// toggleTable();
 function addActiveClass(element) {
+    
     var n;
     for (var i = 0; i < tabContents.length; ++i) {
         tabContents[i].classList.remove("active");
@@ -26,4 +18,30 @@ function addActiveClass(element) {
 
     element.classList.add("active");
     tables[n].classList.add("active");
+}
+
+
+function bmiCalc() {
+    var weight = document.getElementsByClassName('weight')[0].value;
+    var height = document.getElementsByClassName('height')[0].value;
+    var age = document.getElementsByClassName('age')[0].value;
+    var gender = document.getElementsByClassName('gender')[0].value;
+    // console.log(weight, height, age, gender);
+    var bmi = weight / Math.pow(height/100, 2);
+    var status;
+    if (bmi < 18.5) {
+        status = "Underweight";
+    } else if (bmi >= 18.5 && bmi <=24.9) {
+        status = "Normal weight";
+    } else if (bmi >= 25.0 && bmi <=29.9) {
+        status = "Pre-obesity";
+    } else if (bmi >= 30.0 && bmi <=34.9) {
+        status = "Obesity class I";
+    } else if (bmi >= 35.0 && bmi <=39.9) {
+        status = "Obesity class II";
+    } else if (bmi >= 40.0) {
+        status = "Obesity class III";
+    }
+
+    console.log(Math.round(bmi * 10) / 10  + " " + status);
 }
